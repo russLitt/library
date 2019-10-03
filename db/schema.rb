ce4d51d5389c90cb24efd1d8f986f980d92faf9e
Russell Anderson <russellanderson2000@gmail.com>
2019-10-02 23:24:53 -0400

    trying to fix bug with comment foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_010603) do
+ActiveRecord::Schema.define(version: 2019_10_03_031557) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,11 +22,11 @@ ActiveRecord::Schema.define(version: 2019_10_02_010603) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "articles_id", null: false
+    t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["articles_id"], name: "index_comments_on_articles_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  add_foreign_key "comments", "articles", column: "articles_id"
+  add_foreign_key "comments", "articles"
 end
