23e9acaf15e1aa7a3ea8733a563935e80edb7eb3
Russell Anderson <russellanderson2000@gmail.com>
2019-11-23 15:05:23 -0500

    refactoring for Blogs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+resources :blogs do
   resources :articles do
     resources :comments
   end
+end
 
   root 'welcome#index'
 end
